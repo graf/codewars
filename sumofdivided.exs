b5a3b02bcd5d88a3230d48f494d7160414656da0
Pavel Abolmasov <graf@abolmasov.pro>
2020-10-07 12:00:08 +0400

    [4 kyu] https://www.codewars.com/kata/54d496788776e49e6b00052f

--- sumofdivided.exs
+++ sumofdivided.exs
@@ -1,46 +1,9 @@
 ExUnit.start
 
-defmodule PF do
-
-  @doc "Calculates the unique prime factors of a number"
-  def of(num) do
-    prime_factors(abs(num))
-    |> Enum.uniq
-    |> Enum.filter(fn (i) -> i > 1 end)
-  end
-
-  @doc """
-  Calculates all prime factors of a number by finding a low factor
-  and then recursively calculating the factors of the high factor.
-  Skips all evens except 2.
-  Could be further optimized by only using known primes to find factors.
-  """
-  def prime_factors(num , next \\ 2)
-  def prime_factors(num, 2) do
-    cond do
-      rem(num, 2) == 0 -> [2 | prime_factors(div(num, 2))]
-      4 > num          -> [num]
-      true             -> prime_factors(num, 3)
-    end
-  end
-  def prime_factors(num, next) do
-    cond do
-      rem(num, next) == 0 -> [next | prime_factors(div(num, next))]
-      next + next > num   -> [num]
-      true                -> prime_factors(num, next + 2)
-    end
-  end
-
-end
-
 defmodule Sumofdivided do
-  # cheating, for now
-  def sum_of_divided([100000001, 10000000055]) do
-    [{5, 10000000055}, {17, 100000001}, {5882353, 100000001}, {2000000011, 10000000055}]
-  end    
   def sum_of_divided(lst) do
     Enum.reduce(lst, %{}, fn (num, result) ->
-      Enum.reduce(PF.of(num), result, fn (prime, result) ->
+      Enum.reduce(Enum.uniq(factors(num)), result, fn (prime, result) ->
         sum = Map.get(result, prime, 0)
         Map.put(result, prime, sum + num)
       end)
@@ -48,6 +11,11 @@ defmodule Sumofdivided do
     |> Map.to_list
     |> Enum.sort
   end
+
+  def factors(n), do: factors(abs(n), 2, [])
+  def factors(n, k, f) when k * k > n, do: [n | f]
+  def factors(n, k, f) when rem(n, k) == 0, do: factors(div(n, k), k, [k | f])
+  def factors(n, k, f), do: factors(n, k + 1, f)
 end
 
 defmodule SumofdividedTest do
